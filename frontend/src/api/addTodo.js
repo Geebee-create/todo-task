4a3f3c04476af97c00339171ecd6a2018ba0f0fd
Olliefranks <156423860+Olliefranks@users.noreply.github.com>
2024-03-12 15:55:41 +0000

    added to the url and api

--- frontend/src/api/addTodo.js
+++ frontend/src/api/addTodo.js
@@ -1,19 +1,19 @@
-const API_URL = `http://localhost:4000`
+const API_URL = `http://localhost:4000`;
 
 export const addTodo = async (todo) => {
-    // pass paramter to function
-    // create new object with 'text' key (depending on your Model)
-    let obj = {  }
-    const response = await fetch(``, {
-        // method type?
-        method: '',
-        // sending body, stringify data
-        body: JSON.stringify(),
-        // content type?
-        headers: {
-            
-        }
-    })
-    const json = await response.json()
-    return json
-}
+  // pass paramter to function
+  // create new object with 'text' key (depending on your Model)
+  let obj = { text: todo };
+  const response = await fetch("http://localhost:4000/todos/item", {
+    // method type?
+    method: "POST",
+    // sending body, stringify data
+    body: JSON.stringify(obj),
+    // content type?
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const json = await response.json();
+  return json;
+};
--- frontend/src/api/getTodo.js
+++ frontend/src/api/getTodo.js
@@ -1,5 +1,7 @@
-const API_URL = `http://localhost:4000`
+const API_URL = `http://localhost:4000`;
 
 export const getTodo = async (id) => {
-
-}
+  let response = await fetch(`http://localhost:4000/todos/${todo._id}`);
+  let data = await response.json();
+  return data.todos;
+};
