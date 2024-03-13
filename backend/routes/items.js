f0a03679e6bda9f011f498a1200113823f83d8ee
Olliefranks <156423860+Olliefranks@users.noreply.github.com>
2024-03-13 15:19:43 +0000

    back-end done

---
+++ backend/routes/items.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router(); // create a router
+const itemsController = require("../controllers/itemsController");
+
+// routes to controllers
+// the '/items' route will run the getTodos controller
+// which will fetch the data from your database
+router.get("/items", itemsController.getTodos);
+router.get("/item/:id", itemsController.getTodo);
+router.post("/item", itemsController.createTodo);
+router.patch("/item/:id", itemsController.editTodo);
+router.delete("/item/:id", itemsController.deleteTodo);
+module.exports = router;
