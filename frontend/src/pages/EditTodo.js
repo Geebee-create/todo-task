d95acdd233c116fdef807bae3896a77c0d7883a3
Geebee-create <156425632+Geebee-create@users.noreply.github.com>
2024-03-13 11:35:37 +0000

    Work on frontend
    I have worked to complete the pages folder in frontend

--- frontend/src/pages/EditTodo.js
+++ frontend/src/pages/EditTodo.js
@@ -1,38 +1,47 @@
-import { useParams } from "react-router-dom"
-import { useState, useEffect } from 'react'
-import { getTodo } from "../api/getTodo"
-import { updateTodo } from "../api/updateTodo"
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { getTodo } from "../api/getTodo";
+import { updateTodo } from "../api/updateTodo";
+
 const EditTodo = () => {
-
-    const { id } = useParams()
-    const [toUpdate, setToUpdate] = useState('')
-    const [userInput, setUserInput] = useState('')
-
-    const submitHandler = async () => {
-        let obj = {
-            _id: toUpdate._id,
-            text: userInput
-        }
-
-        alert('edited item')
-    }
+    const { id } = useParams();
+    const [toUpdate, setToUpdate] = useState('');
+    const [userInput, setUserInput] = useState('');
 
     useEffect(() => {
         const fetchTodo = async () => {
-            let data = await getTodo(id)
-            setToUpdate(data)
+            try {
+                let data = await getTodo(id);
+                setToUpdate(data);
+            } catch (error) {
+                console.error("Error fetching todo:", error);
+            }
+        };
+        fetchTodo();
+    }, [id]);
+
+    const submitHandler = async () => {
+        try {
+            let updatedTodo = { ...toUpdate, text: userInput }; // This will update the text property
+            await updateTodo(updatedTodo); // This calls the updateTodo function with the updated todo object
+            alert('Todo item updated successfully');
+        } catch (error) {
+            console.error("Error updating todo:", error);
+            alert('Failed to update todo item');
         }
-        fetchTodo()
-    },[])
+    };
+
     return (
         <div>
-            <h1>edit</h1>
+            <h1>Edit</h1>
             <h2>{toUpdate.text}</h2>
             <input 
-                onChange={() => {}}
+                value={userInput}
+                onChange={(e) => setUserInput(e.target.value)}
             />
-            <button onClick={submitHandler}>submit</button>
+            <button onClick={submitHandler}>Submit</button>
         </div>
-    )
-}
-export default EditTodo
+    );
+};
+
+export default EditTodo;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,13 +2,24 @@ import { useEffect, useState } from "react";
 import { readTodos } from "../api/readTodos";
 import Card from '../components/Card'
 import { deleteTodo } from "../api/deleteTodo";
+
+
 const Home = () => {
-    const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState([]);
 
 
-    const deleteHandler = async (todo) => {
-      
+    const deleteHandler = async (todoID) => {
+        try {
+            await deleteTodo(todoID);
+            // Line below will filter out the deleted todo
+            setTodos(prevTodos => prevTodos.filter(todo => todo._id !== todoId));
+        } catch (error) {
+            console.error("Error deleting todo:", error);
+        }
     }
+
+// I don't need to import anything for fetchTodos to work.
+// another try catch could potentially be added in here.
     useEffect(() => {
         const fetchTodos = async () => {
             let data = await readTodos()
@@ -18,18 +29,22 @@ const Home = () => {
         fetchTodos()
     }, [])
 
-    if (!todos) return <h1>loading...</h1>
-    return (
-        <div>
-            <>
-                {
-                    todos ? todos.map((todo) => <Card key={todo._id} deleteHandler={deleteHandler} todo={todo}/>)
-                        : <p>loading...</p>
-                }
-            </>
-        </div>
-    );
 
+// instead of checking for !todos, this checks if the todos.length is 0
+// this is better as !todos might show loading even when the array is null
+    if (todos.length === 0) return <h1>loading...</h1>;
+   
+ 
+return (
+    <div>
+        <>
+            {todos.map((todo) => (
+                // Passes the deleteHandler function as a prop to the Card component
+                <Card key={todo._id} deleteHandler={() => deleteHandler(todo._id)} todo={todo}/>
+            ))}
+        </>
+    </div>
+);
 }
 
 export default Home
